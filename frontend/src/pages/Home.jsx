2dc21c9394505c4a80c01465a016a05505a2a319
Rachel Pastreich <rachpast@bu.edu>
2025-01-16 14:24:48 -0500

    final

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,27 @@
 import React from 'react'
 
 const Home = () => {
+  const navigate = useNavigate()
+  const fetchUser =  async () => {
+    try {
+      const token = localStorage.getItem('token')
+      const response = await axios.get('/http://localhost:3000/auth/home', {
+        headers: {
+          "Authorization": `Bearer ${token}`
+        }
+  })
+  if (response.status !== 201) {
+    navigate('/login')
+  }
+  } catch (err) {
+    navigate('/login')
+    console.log(err)
+  }
+}
+  useEffect(() => {
+    
+  fetchUser()
+  }, [])
   return (
     <div className='text-3xl text-blue-500'>Home</div>
   )
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -45,5 +45,31 @@ router.post('/login', async (req, res) => {
     }
 })
 
+const verifyToken = (req, res, next) => {
+    try {
+        const token = req.headers['authorization'].split(' ')[1]
+        if (!token) {
+            return res.status(403).json({message: "No Token Provided"})
+        }
+        const decoded = jwt.verify(token, process.env.JWT_KEY)
+        req.userId = decoded.id;
+        next()
+    } catch (err) {
+        return res.status(500).json("server error")
+    }
+}
+router.get('/home', verifyToken, async (req, res) => {
+    try {
+        const db = await connectToDatabase()
+        const [rows] = await db.query('SELECT * FROM users WHERE id = ?', [req.userId])
+        if(rows.length === 0) {
+            return res.status(404).json({message : "user not existed"})
+        }
+        return res.status(201).json({user: rows[0]})
+    } catch (err) {
+        return res.status(500).json({message: "server error"})
+    }
+})
+
 
 export default router;
